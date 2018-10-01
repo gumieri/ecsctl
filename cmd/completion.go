191dfb68e7b2b181dc8c2e684ecb4fa5a4663b33
Rafael Gumieri <rafael.gumieri@tecnospeed.com.br>
2018-10-01 17:12:41 -0300

    Create completion command

---
+++ cmd/completion.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"errors"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func completionRun(cmd *cobra.Command, args []string) {
+	switch args[0] {
+	case "bash":
+		rootCmd.GenBashCompletion(os.Stdout)
+	case "zsh":
+		rootCmd.GenZshCompletion(os.Stdout)
+	default:
+		typist.Must(errors.New("specied shell is not yet supported"))
+	}
+}
+
+var completionCmd = &cobra.Command{
+	Use:   "completion [shell]",
+	Short: "Output the completion script for the specified shell language ('bash' or 'zsh')",
+	Args:  cobra.ExactArgs(1),
+	Run:   completionRun,
+}
+
+func init() {
+	rootCmd.AddCommand(completionCmd)
+}
